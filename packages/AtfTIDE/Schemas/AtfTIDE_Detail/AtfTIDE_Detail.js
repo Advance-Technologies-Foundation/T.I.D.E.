030b3d7c137968cc2776d3c7347ddd79dae56791
Kirill Krylov <k.krylov@creatio.com>
2024-11-28 22:21:47 +0100

    init

---
+++ packages/AtfTIDE/Schemas/AtfTIDE_Detail/AtfTIDE_Detail.js
@@ -0,0 +1,8 @@
+define('AtfTIDE_Detail', [], function () {
+	return {
+		entitySchemaName: 'AtfTIDE',
+		details: /**SCHEMA_DETAILS*/ {} /**SCHEMA_DETAILS*/,
+		diff: /**SCHEMA_DIFF*/ [] /**SCHEMA_DIFF*/,
+		methods: {},
+	};
+});
