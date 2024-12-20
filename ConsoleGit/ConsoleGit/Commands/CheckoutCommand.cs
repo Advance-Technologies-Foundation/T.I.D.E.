5306042a6830639782d60cf5b8edfea0b69ba3b6
Vladimir <nikonov.volodymyr@gmail.com>
2024-12-20 03:25:00 +0100

    Add check local branch exists

--- ConsoleGit/ConsoleGit/Commands/CheckoutCommand.cs
+++ ConsoleGit/ConsoleGit/Commands/CheckoutCommand.cs
@@ -6,25 +6,17 @@ namespace ConsoleGit.Commands;
 public class CheckoutCommand(CommandLineArgs args) : BaseRepositoryCommand(args) {
 
 	public override ErrorOr<Success> Execute(){
-		
-		
-		// Get the remote branch
-		var remoteBranch = InitializedRepository.AllBranches[$"origin/{args.BranchName}"];
-    
-		// Create and checkout a new local branch that tracks the remote branch
-		Branch localBranch = InitializedRepository.CreateBranch(args.BranchName, remoteBranch.Tip);
-		Commands.Checkout(repo, localBranch);
-    
-		// Set up tracking information
-		repo.Branches.Update(localBranch,
-			b => b.TrackedBranch = remoteBranch.CanonicalName);
-		
+		var localBranch = InitializedRepository.ListLocalBranches().Value.FirstOrDefault(b => b.FriendlyName == args.BranchName);
+		if (localBranch == null) {
+			var remoteBranch = InitializedRepository.AllBranches[$"origin/{args.BranchName}"];
+			localBranch = InitializedRepository.CreateBranch(args.BranchName, remoteBranch.Tip).Value;
+		}
 		return InitializedRepository
-		       .CheckoutBranch(args.BranchName)
-		       .MatchFirst<ErrorOr<Success>>(
-			       _ => Result.Success,
-			       failure => failure
-		       );
+			   .CheckoutBranch(localBranch.FriendlyName)
+			   .MatchFirst<ErrorOr<Success>>(
+				   _ => Result.Success,
+				   failure => failure
+			   );
 	}
 
 }
--- ConsoleGit/GitAbstraction/GitAbstraction.cs
+++ ConsoleGit/GitAbstraction/GitAbstraction.cs
@@ -6,8 +6,10 @@ using ErrorOr;
 using LibGit2Sharp;
 using LibGit2Sharp.Handlers;
 
-namespace GitAbstraction {
-	public sealed class GitRepository : IDisposable {
+namespace GitAbstraction
+{
+	public sealed class GitRepository : IDisposable
+	{
 
 		#region Fields: Private
 
@@ -17,7 +19,7 @@ namespace GitAbstraction {
 
 		#region Constructors: Private
 
-		private GitRepository(UsernamePasswordCredentials cred, Uri gitUrl, string repoDirectory){
+		private GitRepository(UsernamePasswordCredentials cred, Uri gitUrl, string repoDirectory) {
 			Credentials = cred;
 			GitUrl = gitUrl;
 			FileSystem = new FileSystem();
@@ -42,13 +44,13 @@ namespace GitAbstraction {
 
 		public IDirectoryInfo RepoDirectory { get; }
 
-		private Lazy<Repository> RepositoryLazy => new Lazy<Repository> (()=>new Repository(RepoDirectory.FullName));
+		private Lazy<Repository> RepositoryLazy => new Lazy<Repository>(() => new Repository(RepoDirectory.FullName));
 
 		#endregion
 
 		#region Methods: Private
 
-		private void Dispose(bool disposing){
+		private void Dispose(bool disposing) {
 			if (!_disposed) {
 				if (disposing) {
 					// Dispose managed resources
@@ -66,7 +68,7 @@ namespace GitAbstraction {
 
 		#region Methods: Public
 
-	
+
 		/// <summary>
 		///  Gets an instance of the <see cref="GitRepository"/> class.
 		/// </summary>
@@ -75,8 +77,8 @@ namespace GitAbstraction {
 		/// <param name="gitUrl">The URL of the Git repository.</param>
 		/// <param name="repoDirectory">The directory where the repository will be cloned.</param>
 		/// <returns>A new instance of the <see cref="GitRepository"/> class.</returns>
-		public static GitRepository GetInstance(string username, string password, Uri gitUrl, string repoDirectory){
-			UsernamePasswordCredentials usernamePasswordCredentials = new () {Username = username, Password = password};
+		public static GitRepository GetInstance(string username, string password, Uri gitUrl, string repoDirectory) {
+			UsernamePasswordCredentials usernamePasswordCredentials = new() { Username = username, Password = password };
 			return new GitRepository(usernamePasswordCredentials, gitUrl, repoDirectory);
 		}
 
@@ -87,14 +89,14 @@ namespace GitAbstraction {
 		///  An <see cref="ErrorOr{T}" /> containing the path to the cloned repository or an error.
 		/// </returns>
 		/// <seealso href="https://github.com/libgit2/libgit2sharp/wiki/git-clone">libgit2sharp Wiki Clone</seealso>
-		public ErrorOr<string> Clone(){
+		public ErrorOr<string> Clone() {
 			try {
-				if(RepoDirectory.Exists){
+				if (RepoDirectory.Exists) {
 					RepoDirectory.Delete(true);
-				}else{
+				} else {
 					RepoDirectory.Create();
 				}
-				CloneOptions cloneOptions = new ();
+				CloneOptions cloneOptions = new();
 				if (Credentials == null) {
 					return Repository.Clone(GitUrl.ToString(), RepoDirectory.FullName);
 				}
@@ -105,18 +107,18 @@ namespace GitAbstraction {
 			}
 		}
 
-		public ErrorOr<Success> Fetch(){
+		public ErrorOr<Success> Fetch() {
 			string logMessage = "";
-			
+
 			var remote = InitializedRepository.Network.Remotes["origin"];
 			var refSpecs = remote.FetchRefSpecs.Select(x => x.Specification);
 			Commands.Fetch(InitializedRepository, remote.Name, refSpecs, null, logMessage);
 			Console.Out.WriteLine(logMessage);
 			return Result.Success;
 		}
-		
-		
-		public void Dispose(){
+
+
+		public void Dispose() {
 			Dispose(true);
 			GC.SuppressFinalize(this);
 		}
@@ -128,7 +130,7 @@ namespace GitAbstraction {
 		///  libgit2sharp Wiki
 		///  listing-branches
 		/// </seealso>
-		public ErrorOr<IEnumerable<Branch>> ListLocalBranches(){
+		public ErrorOr<IEnumerable<Branch>> ListLocalBranches() {
 			try {
 				return InitializedRepository.Branches
 					//.Where(b => !b.IsRemote)
@@ -147,7 +149,7 @@ namespace GitAbstraction {
 		/// </returns>
 		/// ///
 		/// <seealso href="https://github.com/libgit2/libgit2sharp/wiki/git-pull">libgit2sharp Wiki git-pull</seealso>
-		public ErrorOr<MergeResult> Pull(){
+		public ErrorOr<MergeResult> Pull() {
 			// Credential information to fetch
 			PullOptions options = new PullOptions() {
 				FetchOptions = new FetchOptions {
@@ -162,7 +164,7 @@ namespace GitAbstraction {
 			// Pull
 			return Commands.Pull(InitializedRepository, signature, options);
 		}
-	
+
 		/// <summary>
 		///  Pushes the specified branch to the remote repository.
 		/// </summary>
@@ -171,12 +173,12 @@ namespace GitAbstraction {
 		///  An <see cref="ErrorOr{T}" /> indicating success or failure.
 		/// </returns>
 		/// <seealso href="https://github.com/libgit2/libgit2sharp/wiki/git-push#git-push">libgit2sharp Wiki git-push</seealso>
-		public ErrorOr<Success> Push(string branchName){
-			Branch branch  = InitializedRepository.Branches[branchName];
+		public ErrorOr<Success> Push(string branchName) {
+			Branch branch = InitializedRepository.Branches[branchName];
 			return branch == null ? Error.Failure("BranchNotFound", $"Branch {branchName} not found") : Push(branch);
 		}
-	
-	
+
+
 		/// <summary>
 		///  Pushes the specified branch to the remote repository.
 		/// </summary>
@@ -185,24 +187,24 @@ namespace GitAbstraction {
 		///  An <see cref="ErrorOr{T}" /> indicating success or failure.
 		/// </returns>
 		/// <seealso href="https://github.com/libgit2/libgit2sharp/wiki/git-push#git-push">libgit2sharp Wiki git-push</seealso>
-		public ErrorOr<Success> Push(Branch branch){
-			PushOptions options = new () {
+		public ErrorOr<Success> Push(Branch branch) {
+			PushOptions options = new() {
 				CredentialsProvider = CredentialsProvider
 			};
 			//Remote remote = InitializedRepository.Network.Remotes["origin"];
-			
+
 			InitializedRepository.Network.Push(branch, options);
 			//InitializedRepository.Network.Push(remote, $"refs/heads/{branch.FriendlyName}", options);
 			return Result.Success;
 		}
-		
+
 		/// <summary>
 		/// Checkout to existing branch by name
 		/// </summary>
 		/// <param name="branchName">Branch name to checkout</param>
 		/// <returns></returns>
 		/// <seealso href="https://github.com/libgit2/libgit2sharp/wiki/git-checkout#checkout-to-existing-branch-by-name">libgit2sharp Wiki git checkout <c>branch</c></seealso>
-		public ErrorOr<Success> CheckoutBranch(string branchName){
+		public ErrorOr<Success> CheckoutBranch(string branchName) {
 			Branch firstOrDefaultBranch = InitializedRepository.Branches
 				.Where(b => b.FriendlyName == branchName)
 				.Select(b => b)
@@ -211,37 +213,37 @@ namespace GitAbstraction {
 			if (firstOrDefaultBranch == null) {
 				return Error.Failure("BranchNotFound", $"Branch {branchName} not found");
 			}
-			
+
 			Branch result = Commands.Checkout(InitializedRepository, firstOrDefaultBranch);
 			if (result == null) {
 				return Error.Failure("BranchNotFound", $"Branch {branchName} not found");
 			}
 			return Result.Success;
 		}
-		
+
 		/// <summary>
 		///  Stage all working directory changes
 		///  Adds all files to the staging area.
 		/// </summary>
 		/// <remarks>git add --all .</remarks>
 		/// <seealso href="https://github.com/libgit2/libgit2sharp/wiki/git-add#stage-all-working-directory-changes">libgit2sharp Wiki git add</seealso>
-		public ErrorOr<Success> AddAll(){
+		public ErrorOr<Success> AddAll() {
 			Commands.Stage(InitializedRepository, "*");
 			return Result.Success;
 		}
-		
+
 		/// <summary>
 		/// Stage (add) a file in a repository that is checked out to the file system:
 		/// </summary>
 		/// <param name="filePath">Filepath to a file with changes, relative to .git directory</param>
 		/// <returns></returns>
 		/// <seealso href="https://github.com/libgit2/libgit2sharp/wiki/git-add#add-to-a-non-bare-repository">libgit2sharp Wiki git add</seealso>
-		public ErrorOr<Success> AddFile(string filePath){
+		public ErrorOr<Success> AddFile(string filePath) {
 			InitializedRepository.Index.Add(filePath);
 			InitializedRepository.Index.Write();
 			return Result.Success;
-		} 
-		
+		}
+
 		/// <summary>
 		/// Make a commit to a non-bare repository
 		/// </summary>
@@ -250,26 +252,26 @@ namespace GitAbstraction {
 		/// <param name="message">Commit message</param>
 		/// <returns><see cref="Commit"/></returns>
 		/// <seealso href="https://github.com/libgit2/libgit2sharp/wiki/git-commit#make-a-commit-to-a-non-bare-repository">libgit2sharp Wiki git commit</seealso>
-		public ErrorOr<Commit> Commit(string name, string email, string message){
+		public ErrorOr<Commit> Commit(string name, string email, string message) {
 			// Create the committer's signature and commit
-			
-			Signature author = new (name, email, DateTime.Now);
+
+			Signature author = new(name, email, DateTime.Now);
 
 			// Commit to the repository
 			//INVESTIGATE: We can potentially pass TIDE as committer
-			
+
 			Commit commit = InitializedRepository.Commit(message, author, author, new CommitOptions {
 				PrettifyMessage = false
 			});
 			return commit;
 		}
-		
-		
-		public ErrorOr<IEnumerable<Commit>> GetCommits(){
-		
+
+
+		public ErrorOr<IEnumerable<Commit>> GetCommits() {
+
 			return InitializedRepository.Commits.ToList();
 		}
-	
+
 		/// <summary>
 		///  Creates a new branch pointing at the current HEAD.
 		/// </summary>
@@ -278,31 +280,30 @@ namespace GitAbstraction {
 		///  An <see cref="ErrorOr{T}" /> indicating success or failure.
 		/// </returns>
 		/// <seealso href="https://github.com/libgit2/libgit2sharp/wiki/git-branch#creating-a-branch-pointing-at-the-current-head">libgit2sharp Wiki</seealso>
-		public ErrorOr<Branch> CreateBranch(string branchName){
+		public ErrorOr<Branch> CreateBranch(string branchName, Commit tip = null) {
 			try {
-				return InitializedRepository.CreateBranch(branchName);
+				return tip == null ? InitializedRepository.CreateBranch(branchName) : InitializedRepository.CreateBranch(branchName, tip);
 			} catch (Exception e) {
 				return e.ToError();
 			}
 		}
-		
-		}
-		
-		public ErrorOr<Success> PublishBranch(Branch branch){
+
+
+		public ErrorOr<Success> PublishBranch(Branch branch) {
 			InitializedRepository.Branches.Update(branch);
 			return Push(branch);
 		}
-		public ErrorOr<Success> CreateBranchAndPublish(string branchName){
+		public ErrorOr<Success> CreateBranchAndPublish(string branchName) {
 			ErrorOr<Branch> branchOrError = CreateBranch(branchName);
-		
-			if(branchOrError.IsError){
+
+			if (branchOrError.IsError) {
 				return branchOrError.Errors;
 			}
 			Branch branch = branchOrError.Value;
 			InitializedRepository.Branches.Update(branch);
 			return Push(branch);
 		}
-		public ErrorOr<Success> DeleteBranch(string branchName){
+		public ErrorOr<Success> DeleteBranch(string branchName) {
 			Branch branches = InitializedRepository.Branches[branchName]; //error
 			if (branches == null) {
 				return Error.Failure("BranchNotFound", $"Branch {branchName} not found");
@@ -310,11 +311,11 @@ namespace GitAbstraction {
 			InitializedRepository.Branches.Remove(branches);
 			return Result.Success;
 		}
-	
-	
+
+
 		#endregion
 
-		~GitRepository(){
+		~GitRepository() {
 			Dispose(false);
 		}
 
