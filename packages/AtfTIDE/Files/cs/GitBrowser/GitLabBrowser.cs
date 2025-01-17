aed73476c9c09e25f17c8a4ee44e82bc4a35be26
P.Shvedun <P.Shvedun@creatio.com>
2025-01-17 02:00:29 +0200

    rework GetAllRepositoriesAsync

--- packages/AtfTIDE/Files/cs/GitBrowser/GitLabBrowser.cs
+++ packages/AtfTIDE/Files/cs/GitBrowser/GitLabBrowser.cs
@@ -1,9 +1,9 @@
 ﻿using Newtonsoft.Json.Linq;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace AtfTIDE.cs.GitBrowser
@@ -11,59 +11,103 @@ namespace AtfTIDE.cs.GitBrowser
 	public class Repository {
 		public string Name { get; set; }
 
-		public string Url { get; set; }
+		public string UrlToClone { get; set; }
+		public string WebUrl { get; set; }
+		public string Id
+		{
+			get;
+			internal set;
+		}
 	}
 
 	public class GitLabBrowser
 	{
 		private string gitlabUrl;
 
+		public int MaxRepositoryCount { get; set; } = int.MaxValue;
+
 		public GitLabBrowser(string gitlabUrl) {
 			this.gitlabUrl = gitlabUrl;
 		}
 
 		public async Task<List<Repository>> GetAllRepositoriesAsync(string gitlabToken) {
-			var repositories = new List<Repository>();
+			var repositories = new ConcurrentBag<Repository>();
 			using (var client = new HttpClient()) {
 				client.DefaultRequestHeaders.Add("Private-Token", gitlabToken);
-				var response = await client.GetAsync($"{gitlabUrl}/api/v4/projects?per_page=100");
-				if (response.IsSuccessStatusCode) {
-					var content = await response.Content.ReadAsStringAsync();
-					var json = JArray.Parse(content);
-					foreach (var repo in json) {
-						repositories.Add(new Repository {
-							Name = repo["name"].ToString(),
-							Url = repo["web_url"].ToString()
-						});
+				int page = 1;
+				bool hasMorePages = true;
+				var tasks = new List<Task>();
+				while (hasMorePages && repositories.Count < MaxRepositoryCount) {
+					var response = await client.GetAsync($"{gitlabUrl}/api/v4/projects?per_page=100&page={page}");
+					if (response.IsSuccessStatusCode) {
+						var content = await response.Content.ReadAsStringAsync();
+						var json = JArray.Parse(content);
+						if (json.Count == 0) {
+							hasMorePages = false;
+						} else {
+							tasks.Add(Task.Run(() => {
+								foreach (var repo in json) {
+									repositories.Add(new Repository {
+										Name = repo["name"].ToString(),
+										UrlToClone = repo["http_url_to_repo"].ToString(),
+										WebUrl = repo["web_url"].ToString(),
+										Id = repo["id"].ToString()
+									});
+								}
+							}));
+							page++;
+						}
+					} else {
+						throw new Exception("Failed to fetch repositories");
 					}
-				} else {
-					throw new Exception("Failed to fetch repositories");
 				}
+				await Task.WhenAll(tasks);
 			}
-			return repositories;
+			return repositories.ToList();
 		}
+
 
 		public List<string> GetAllRepositoriesNames(string gitlabToken) {
 			var repo =  GetAllRepositoriesAsync(gitlabToken).Result;
 			return repo.Select(r => r.Name).ToList();
 		}
 
-		internal List<Repository> FindClioRepositoriesOnServer(string gitlabToken) {
+		internal IEnumerable<Repository> FindClioRepositoriesOnServer(string gitlabToken) {
 			var repositories = GetAllRepositoriesAsync(gitlabToken).Result;
-			List<Repository> clioRepositories = new List<Repository>();
-			foreach(var repo in repositories) {
-				if (IsClioRepositories(repo,gitlabToken)) {
-					clioRepositories.Add(repo);
-				}
-			}
+			ConcurrentBag<Repository> clioRepositories = new ConcurrentBag<Repository>();
+			Parallel.ForEach(repositories, new ParallelOptions() {
+				MaxDegreeOfParallelism = Environment.ProcessorCount } , (repo) => {
+					if (IsClioRepositories(repo,gitlabToken)) {
+						clioRepositories.Add(repo);
+					}
+				});
 			return clioRepositories;
 		}
 
-		private bool IsClioRepositories(Repository repo, string gitlabToken) {
+		public bool IsClioRepositories(Repository repo, string gitlabToken) {
 			using (var client = new HttpClient()) {
 				client.DefaultRequestHeaders.Add("Private-Token", gitlabToken);
-				var response = client.GetAsync($"{repo.Url}/repository/tree?path=.clio").Result;
-				return response.IsSuccessStatusCode;
+				if (string.IsNullOrEmpty(repo.Id)) {
+					var response = client.GetAsync($"{gitlabUrl}/api/v4/projects?search={repo.Name}").Result;
+					if (response.IsSuccessStatusCode) {
+						var content = response.Content.ReadAsStringAsync().Result;
+						var json = JArray.Parse(content);
+						if (json.Count > 0) {
+							repo.Id = json[0]["id"].ToString();
+						} else {
+							return false;
+						}
+					} else {
+						return false;
+					}
+				}
+				var repoResponse = client.GetAsync($"{gitlabUrl}/api/v4/projects/{repo.Id}/repository/tree?path=.clio").Result;
+				if (repoResponse.IsSuccessStatusCode) {
+					var repoContent = repoResponse.Content.ReadAsStringAsync().Result;
+					var repoJson = JArray.Parse(repoContent);
+					return repoJson.Count > 0;
+				}
+				return false;
 			}
 		}
 	}
--- tests/AtfTIDE/GitBrowser/GitLabBrowser.Tests.cs
+++ tests/AtfTIDE/GitBrowser/GitLabBrowser.Tests.cs
@@ -1,10 +1,10 @@
 ﻿using AtfTIDE.cs.GitBrowser;
 using FluentAssertions;
+using Newtonsoft.Json;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace AtfTIDE.Tests.GitBrowser
@@ -12,20 +12,88 @@ namespace AtfTIDE.Tests.GitBrowser
 	[TestFixture]
 	internal class GitLabBrowserTests
 	{
+		private static string FindConfigFilePath(string fileName) {
+			string directory = AppDomain.CurrentDomain.BaseDirectory;
+			while (directory != null) {
+				string filePath = Path.Combine(directory, fileName);
+				if (File.Exists(filePath)) {
+					return filePath;
+				}
+				directory = Directory.GetParent(directory)?.FullName;
+			}
+			throw new FileNotFoundException($"Configuration file '{fileName}' not found.");
+		}
+
+		private static string ConfigFilePath = FindConfigFilePath("Tide.settings");
+
+		private static dynamic LoadConfig() {
+			using (StreamReader r = new StreamReader(ConfigFilePath)) {
+				string json = r.ReadToEnd();
+				return JsonConvert.DeserializeObject(json);
+			}
+		}
+
+		private static readonly dynamic Config = LoadConfig();
+		private static readonly string GitlabUrl = Config.GitlabUrl;
+		private static readonly string GitlabToken = Config.GitlabToken;
+		private static readonly string StudioUrl = Config.StudioUrl;
+		private static readonly string StudioFreeThemeUrl = Config.StudioFreeThemeUrl;
+
 		[Test]
 		public async Task FindAllRepositoriesOnServer() {
-			
-			var gitlabBrowser = new GitLabBrowser(gitlabUrl);
-			List<string> repositories = gitlabBrowser.GetAllRepositoriesNames(gitlabToken);
+			var gitlabBrowser = new GitLabBrowser(GitlabUrl);
+			List<string> repositories = gitlabBrowser.GetAllRepositoriesNames(GitlabToken);
 			Assert.Contains("labKirill", repositories);
 		}
 
 		[Test]
 		public async Task FindClioWorkspacesOnServer() {
+			var gitlabBrowser = new GitLabBrowser(GitlabUrl);
+			gitlabBrowser.MaxRepositoryCount = 100;
+			var repositories = gitlabBrowser.FindClioRepositoriesOnServer(GitlabToken);
+			repositories.Should().Contain(r => r.Name == "labKirill"
+				&& r.UrlToClone.Contains("k.krylov/labkirill.git")
+				&& r.WebUrl.Contains("k.krylov/labkirill"));
+		}
 
-			var gitlabBrowser = new GitLabBrowser(gitlabUrl);
-			List<Repository> repositories = gitlabBrowser.FindClioRepositoriesOnServer(gitlabToken);
-			repositories.Should().Contain(r => r.Name == "labKirill");
+		[Test]
+		public async Task FindClioWorkspacesOnServer_ShouldBeFalse() {
+			var gitlabBrowser = new GitLabBrowser(GitlabUrl);
+			gitlabBrowser.MaxRepositoryCount = 100;
+			Repository repository = new Repository();
+			repository.WebUrl = StudioFreeThemeUrl;
+			repository.Name = "studio-free-theme";
+			Assert.IsFalse(gitlabBrowser.IsClioRepositories(repository, GitlabToken));
+		}
+
+		[Test]
+		public async Task FindClioWorkspacesOnServerByName() {
+			var gitlabBrowser = new GitLabBrowser(GitlabUrl);
+			gitlabBrowser.MaxRepositoryCount = 100;
+			Repository repository = new Repository();
+			repository.WebUrl = StudioUrl;
+			repository.Name = "labkirill";
+			Assert.IsTrue(gitlabBrowser.IsClioRepositories(repository, GitlabToken));
+		}
+
+		[Test]
+		public async Task FindClioWorkspacesOnServerById() {
+			var gitlabBrowser = new GitLabBrowser(GitlabUrl);
+			gitlabBrowser.MaxRepositoryCount = 100;
+			Repository repository = new Repository();
+			repository.WebUrl = StudioUrl;
+			repository.Id = "1342";
+			Assert.IsTrue(gitlabBrowser.IsClioRepositories(repository, GitlabToken));
+		}
+
+		[Test]
+		public async Task FindClioWorkspacesOnServerByIdShouldBeFalse() {
+			var gitlabBrowser = new GitLabBrowser(GitlabUrl);
+			gitlabBrowser.MaxRepositoryCount = 100;
+			Repository repository = new Repository();
+			repository.WebUrl = StudioUrl;
+			repository.Id = "239";
+			Assert.IsFalse(gitlabBrowser.IsClioRepositories(repository, GitlabToken));
 		}
 	}
 }
