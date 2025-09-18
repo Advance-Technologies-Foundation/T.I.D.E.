9d9d823d8ab374a5bb751a5f82d7b5fd3029fa46
Vladimir <nikonov.volodymyr@gmail.com>
2025-09-19 01:17:13 +0200

    chore: local changes before release

--- scripts/release.cmd
+++
@@ -1,3 +0,0 @@
-call set-version.cmd %1
-call pack-nuget-pkg.cmd
-call publish-tide.cmd %1 %2
--- scripts/release.sh
+++ scripts/release.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
-./set-version.sh "$1"
+./release-tide-github.sh
+
 ./pack-nuget-pkg.sh
-./publish-tide.sh "$1" "$2"
+# Read version from descriptor.json
+echo "--- Starting GitHub release ---"
+./release-tide-github.sh
+
+echo "--- Packing NuGet package ---"
+./pack-nuget-pkg.sh
+
+echo "--- Reading version from descriptor.json ---"
+version=$(jq -r '.Descriptor.PackageVersion' ../packages/AtfTIDE/descriptor.json)
+echo "NuGet publish version: $version"
+
+echo "--- Publishing TIDE to NuGet ---"
+./publish-tide-to-nuget.sh "$version" "$1"
